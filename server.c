e44d7620e4ded438a78263d3c8919668648024cd
Avery2 <justinaverychan@gmail.com>
2021-04-29 22:55:28 -0500

    8/15; cv fail; added command line parsing, multi-threading test user program

--- server.c
+++ server.c
@@ -12,13 +12,16 @@
 //
 
 // CS537: Parse the new arguments too
-void getargs(int *port, int argc, char *argv[])
+void getargs(int *port, int *threads, int *buffers, char** shm_name, int argc, char *argv[])
 {
-  if (argc != 2) {
+  if (argc != 5) {
     fprintf(stderr, "Usage: %s <port>\n", argv[0]);
     exit(1);
   }
   *port = atoi(argv[1]);
+  *threads = atoi(argv[2]);
+  *buffers = atoi(argv[3]);
+  *shm_name = argv[4];
 }
 
 
@@ -26,8 +29,14 @@ int main(int argc, char *argv[])
 {
   int listenfd, connfd, port, clientlen;
   struct sockaddr_in clientaddr;
+  // new cl args
+  int threads, buffers;
+  char* shm_name;
 
-  getargs(&port, argc, argv);
+  getargs(&port, &threads, &buffers, &shm_name, argc, argv);
+  // test values for threads and buffer are positve integers
+  if (threads < 0 || buffers < 0 || port < 22)
+	  exit(1);
 
   //
   // CS537 (Part B): Create & initialize the shared memory region...
